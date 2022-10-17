bba618cab6c8b40addc30ef87d2363b90b28b5e3
Davirain <davirain.yin@gmail.com>
2022-10-17 14:55:16 +0800

    fix standard library types

--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -9,9 +9,6 @@
 // Execute `rustlings hint iterators5` for hints.
 //
 // Make the code compile and the tests pass.
-
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -34,6 +31,14 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
+    let sum = map.iter().fold(0, |acc, (_, val)| {
+        if val == &value {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+    sum
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -52,6 +57,10 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+    let sum = collection.iter().fold(0, |acc, val| {
+        acc + count_iterator(val, value)
+    });
+    sum
 }
 
 #[cfg(test)]
